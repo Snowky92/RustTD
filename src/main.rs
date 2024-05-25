d898bb5f0c03d652f4a31ec22fb78a340c876b7f
Snowky92 <amaurypinheiro@hotmail.fr>
2024-05-25 16:04:50 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, spawn_camera)
+        .add_systems(Startup, spawn_grid)
         .run();
 }
 
@@ -26,4 +27,26 @@ pub fn spawn_camera(
             ..default()
         }
     );
+}
+
+pub fn spawn_grid(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>
+) {
+    let window = window_query.get_single().unwrap();
+    let cell_size = 50.0;
+
+    for y in (0..(window.height() as i32)).step_by(cell_size as usize) {
+        for x in (0..(window.width() as i32)).step_by(cell_size as usize) {
+            commands.spawn(SpriteBundle {
+                sprite: Sprite {
+                    color: Color::rgba(0.9, 0.9, 0.9, 0.5),
+                    custom_size: Some(Vec2::new(cell_size - 1.0, cell_size - 1.0)),
+                    ..default()
+                },
+                transform: Transform::from_translation(Vec3::new(x as f32, y as f32, 0.0)),
+                ..default()
+            });
+        }
+    }
 }
