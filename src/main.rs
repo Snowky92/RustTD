640de6c75afb545e4a6fe501358ef042d7971df1
Snowky92 <amaurypinheiro@hotmail.fr>
2024-05-26 02:44:24 +0200

    ajout object on click

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, spawn_camera)
         .add_systems(Startup, spawn_grid)
+        .add_systems(Update, handle_mouse_clicks)
         .run();
 }
 
@@ -47,6 +48,35 @@ pub fn spawn_grid(
                 transform: Transform::from_translation(Vec3::new(x as f32, y as f32, 0.0)),
                 ..default()
             });
+        }
+    }
+}
+
+pub fn handle_mouse_clicks(
+    mut commands: Commands,
+    mouse_button_input: Res<ButtonInput<MouseButton>>,
+    windows: Query<&Window, With<PrimaryWindow>>,
+    q_camera: Query<(&Camera, &GlobalTransform)>
+) {
+    if mouse_button_input.just_pressed(MouseButton::Left) {
+        let window = windows.get_single().unwrap();
+        if let Some(cursor_position) = window.cursor_position() {
+            for (camera, camera_transform) in q_camera.iter() {
+                let window_size = Vec2::new(window.width() as f32, window.height() as f32);
+                let ndc = (cursor_position / window_size) * 2.0 - Vec2::ONE;
+                let ndc_to_world = camera_transform.compute_matrix() * camera.projection_matrix().inverse();
+                let world_position = ndc_to_world.project_point3(ndc.extend(-1.0)).truncate();
+
+                commands.spawn(SpriteBundle {
+                    sprite: Sprite {
+                        color: Color::rgb(0.0, 0.0, 1.0),
+                        custom_size: Some(Vec2::new(30.0, 30.0)),
+                        ..default()
+                    },
+                    transform: Transform::from_translation(Vec3::new(world_position.x, -world_position.y, 0.0)),
+                    ..default()
+                });
+            }
         }
     }
 }
