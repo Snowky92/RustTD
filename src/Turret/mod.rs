d303b5e370bad9544bf7c7216707c1dbee49ec44
Snowky92 <amaurypinheiro@hotmail.fr>
2024-06-13 18:21:38 +0200

    Update mod.rs

--- src/Turret/mod.rs
+++ src/Turret/mod.rs
@@ -31,11 +31,11 @@ pub const TURRET_SIZE: f32 = 50.0;
 pub const TURRET_F_COST: i32 = 30;
 pub const BULLET_SPEED_F: f32 = ENEMY_SPEED * 10.0;
 pub const REACH_F: f32 = 200.0;
-pub const BULLET_DAMAGE_F: f32 = 20.0;
+pub const BULLET_DAMAGE_F: f32 = 15.0;
 pub const COOLDOWN_F: f32 = 0.2;
 
 pub const TURRET_S_COST: i32 = 50;
 pub const BULLET_SPEED_S: f32 = ENEMY_SPEED * 10.0;
 pub const REACH_S: f32 = 300.0;
-pub const BULLET_DAMAGE_S: f32 = 80.0;
+pub const BULLET_DAMAGE_S: f32 = 70.0;
 pub const COOLDOWN_S: f32 = 1.0;
